8ff907de964b18669d1db7c198a67396bdd8bac7
ck3908 <ck3908@gmail.com>
2019-11-07 23:09:47 -0500

    made into maven

---
+++ Project Zero CarMaven/src/main/java/CarDetails.java
@@ -0,0 +1,106 @@
+package src.main.java;
+
+public class CarDetails {
+
+	private String carName;
+	private int plate; // unique key
+	private String owned; // flag say aaa if not owned and then owner name if it is
+	private float selling_price;  // if not currently owned, it is offer price. If owned, it is sold price
+	private float downpayment;  //may not need this
+	private float totalPayments;
+	private int financingDeal;  // assume 3 packages 
+	private float monthlyPmt;
+	private int termRemaining;
+	private float prinBal;
+	
+	public CarDetails() {
+		this.carName = " ";
+		this.plate = 0;
+		this.owned = "aaa";
+		this.selling_price = 0;
+		this.downpayment = 0;
+		this.totalPayments = 0;
+		this.financingDeal = 1;  //1 ,2 or 3
+		this.monthlyPmt = 0;
+		this.termRemaining = 0;
+		this.prinBal = 0;
+	}
+	
+	
+	public CarDetails(String carName, int plate, String owned, float selling_price, float downpayment,
+			float totalPayments, int financingDeal, float monthlyPmt, int termRemaining, float prinBal) {
+		super();
+		this.carName = carName;
+		this.plate = plate;
+		this.owned = owned;  // if not zero, the number key references to the owner's name
+		this.selling_price = selling_price;
+		this.downpayment = downpayment;
+		this.totalPayments = totalPayments;
+		this.financingDeal = financingDeal;
+		this.monthlyPmt = monthlyPmt;
+		this.termRemaining = termRemaining;
+		this.prinBal = prinBal;
+	}
+	public String getCarName() {
+		return carName;
+	}
+	public void setCarName(String carName) {
+		this.carName = carName;
+	}
+	public int getPlate() {
+		return plate;
+	}
+	public void setPlate(int plate) {
+		this.plate = plate;
+	}
+	public String getOwned() {
+		return owned;
+	}
+	public void setOwned(String owned) {
+		this.owned = owned;
+	}
+	public float getSelling_price() {
+		return selling_price;
+	}
+	public void setSelling_price(float selling_price) {
+		this.selling_price = selling_price;
+	}
+	public float getDownpayment() {
+		return downpayment;
+	}
+	public void setDownpayment(float downpayment) {
+		this.downpayment = downpayment;
+	}
+	public float getTotalPayments() {
+		return totalPayments;
+	}
+	public void setTotalPayments(float totalPayments) {
+		this.totalPayments = totalPayments;
+	}
+	public int getFinancingDeal() {
+		return financingDeal;
+	}
+	public void setFinancingDeal(int financingDeal) {
+		this.financingDeal = financingDeal;
+	}
+	public float getMonthlyPmt() {
+		return monthlyPmt;
+	}
+	public void setMonthlyPmt(float monthlyPmt) {
+		this.monthlyPmt = monthlyPmt;
+	}
+	public int getTermRemaining() {
+		return termRemaining;
+	}
+	public void setTermRemaining(int termRemaining) {
+		this.termRemaining = termRemaining;
+	}
+	public float getPrinBal() {
+		return prinBal;
+	}
+	public void setPrinBal(float prinBal) {
+		this.prinBal = prinBal;
+	}
+	
+	
+}
